031a5763c8d81731bd2348d2c3d46ad9d17b4875
danielledelgado <159454187+danielledelgado@users.noreply.github.com>
2024-12-16 14:52:11 -0500

    Add files via upload

--- timeZones.cpp
+++ timeZones.cpp
@@ -13,8 +13,31 @@ int main( ){
 
 
    // EXCERCISE ONE
-   time_t now = time(nullptr);
+   time_t now = time(nullptr); // gets the current time
    cout << "Current time: " << ctime(&now);
+
+   // EXCERCISE TWO
+   int timeZoneOffset;
+   cout << "Enter a timezone offset: ";
+   cin >> timeZoneOffset; // puts a number into timezoneoffset variable
+
+   now += timeZoneOffset * 3600; // 60 secs in a minute, 60 minutes in an hour
+
+   tm* timeZoneTime = gmtime(&now); // gets the time of that number
+
+   cout << "Current time in that offset: " << asctime(timeZoneTime);
+
+   // EXCERCISE THREE
+   int answer;
+   time_t beginning; // gets the beginning time
+   time_t end; // gets the ending time
+
+   time(&beginning); // gets the time
+    cout << "what's 2+2" << endl;
+    cin >> answer;
+   time(&end); // gets the time
+
+   cout << "it took you " << difftime(end, beginning) << " seconds to answer"; // uses difftime to calculate the amount of seconds it took
 
     return 0;
 }
